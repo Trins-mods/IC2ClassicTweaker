9091b9565f602fcae2d34402dc77863c91dd0d6e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-03-24 14:59:48 -0400

    added electrolyzer and sawmill support
    also changed mappings version because ic2 classic doesn't like stable_39

---
+++ src/main/java/trinsdar/ic2classictweaker/ClassicElectrolyzerSupport.java
@@ -0,0 +1,102 @@
+package trinsdar.ic2classictweaker;
+
+import crafttweaker.IAction;
+import crafttweaker.annotations.ModOnly;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import ic2.api.classic.recipe.ClassicRecipes;
+import net.minecraft.item.ItemStack;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+import java.util.Locale;
+
+@ModOnly("ic2")
+@ZenClass("mods.ic2.ClassicElectrolyzer")
+@ZenRegister
+public class ClassicElectrolyzerSupport {
+    @ZenMethod
+    public static void addBothRecipe(IItemStack output, IItemStack input, int energy) {
+        CraftTweakerActions.apply(new ElectrolyzerBothRecipeAction(CraftTweakerMC.getItemStack(input), CraftTweakerMC.getItemStack(output), energy));
+    }
+
+    @ZenMethod
+    public static void addChargeRecipe(IItemStack output, IItemStack input, int energy) {
+        CraftTweakerActions.apply(new ElectrolyzerChargeRecipeAction(CraftTweakerMC.getItemStack(input), CraftTweakerMC.getItemStack(output), energy));
+    }
+
+    @ZenMethod
+    public static void addDischargeRecipe(IItemStack output, IItemStack input, int energy) {
+        CraftTweakerActions.apply(new ElectrolyzerDischargeRecipeAction(CraftTweakerMC.getItemStack(input), CraftTweakerMC.getItemStack(output), energy));
+    }
+
+    private static final class ElectrolyzerBothRecipeAction implements IAction {
+
+        private final ItemStack input;
+        private final ItemStack output;
+        private final int charge;
+
+        ElectrolyzerBothRecipeAction(ItemStack input, ItemStack output, int charge) {
+            this.input = input;
+            this.output = output;
+            this.charge = charge;
+        }
+
+        @Override
+        public void apply() {
+            ClassicRecipes.electrolyzer.addBothRecipe(input, output, charge, input.getDisplayName());
+        }
+
+        @Override
+        public String describe() {
+            return String.format(Locale.ENGLISH, "Add Recipe[%s, %s -> %s] to %s", this.input, this.output, ClassicRecipes.sawMill);
+        }
+    }
+
+    private static final class ElectrolyzerChargeRecipeAction implements IAction {
+
+        private final ItemStack input;
+        private final ItemStack output;
+        private final int charge;
+
+        ElectrolyzerChargeRecipeAction(ItemStack input, ItemStack output, int charge) {
+            this.input = input;
+            this.output = output;
+            this.charge = charge;
+        }
+
+        @Override
+        public void apply() {
+            ClassicRecipes.electrolyzer.addChargeRecipe(input, output, charge, input.getDisplayName());
+        }
+
+        @Override
+        public String describe() {
+            return String.format(Locale.ENGLISH, "Add Recipe[%s, %s -> %s] to %s", this.input, this.output, ClassicRecipes.sawMill);
+        }
+    }
+
+    private static final class ElectrolyzerDischargeRecipeAction implements IAction {
+
+        private final ItemStack input;
+        private final ItemStack output;
+        private final int charge;
+
+        ElectrolyzerDischargeRecipeAction(ItemStack input, ItemStack output, int charge) {
+            this.input = input;
+            this.output = output;
+            this.charge = charge;
+        }
+
+        @Override
+        public void apply() {
+            ClassicRecipes.electrolyzer.addDischargeRecipe(input, output, charge, input.getDisplayName());
+        }
+
+        @Override
+        public String describe() {
+            return String.format(Locale.ENGLISH, "Add Recipe[%s, %s -> %s] to %s", this.input, this.output, ClassicRecipes.sawMill);
+        }
+    }
+}
---
+++ src/main/java/trinsdar/ic2classictweaker/SawmillSupport.java
@@ -0,0 +1,47 @@
+package trinsdar.ic2classictweaker;
+
+import crafttweaker.IAction;
+import crafttweaker.annotations.ModOnly;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IIngredient;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import ic2.api.classic.recipe.ClassicRecipes;
+import ic2.api.recipe.IRecipeInput;
+import net.minecraft.item.ItemStack;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+import java.util.Locale;
+
+@ModOnly("ic2")
+@ZenClass("mods.ic2.Sawmill")
+@ZenRegister
+public class SawmillSupport {
+    @ZenMethod
+    public static void addRecipe(IItemStack output, IIngredient input) {
+        CraftTweakerActions.apply(new SawmillRecipeAction(IC2RecipeInputs.of(input),CraftTweakerMC.getItemStack(output)));
+    }
+
+    private static final class SawmillRecipeAction implements IAction {
+
+        private final IRecipeInput input;
+        private final ItemStack output;
+
+        SawmillRecipeAction(IRecipeInput input, ItemStack output) {
+            this.input = input;
+            this.output = output;
+        }
+
+        @Override
+        public void apply() {
+            ClassicRecipes.sawMill.addRecipe(this.input, this.output, 0.0F, input.getInputs().get(0).getDisplayName());
+        }
+
+        @Override
+        public String describe() {
+            return String.format(Locale.ENGLISH, "Add Recipe[%s, %s -> %s] to %s", this.input, this.output, ClassicRecipes.sawMill);
+        }
+    }
+
+}
