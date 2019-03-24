d8ac79d60f1a25264689fff240ededa552aa6093
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-03-24 13:19:58 -0400

    Initial additon of mod files
    Some stuff has been grabbed from Ic2 tweaker. will be changed later to fit this mod.

---
+++ src/main/java/trinsdar/ic2classictweaker/IC2ClassicTweaker.java
@@ -0,0 +1,10 @@
+package trinsdar.ic2classictweaker;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = IC2ClassicTweaker.MODID, name = IC2ClassicTweaker.MODNAME, version = IC2ClassicTweaker.VERSION, dependencies = "required-after:ic2;required-after:ic2-classic-spmod;required-after:crafttweaker;before:jei")
+public class IC2ClassicTweaker {
+    public static final String MODID = "ic2_classic_tweaker";
+    public static final String MODNAME = "IC2 Classic Tweaker";
+    public static final String VERSION = "@VERSION@";
+}
