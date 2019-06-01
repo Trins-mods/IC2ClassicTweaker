969fd1a1f3142deeced4820e1ade72490da1c631
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-06-01 00:36:19 -0400

    work on config for removing some recipes unable to be removed by ct methods

--- src/main/java/trinsdar/ic2classictweaker/IC2ClassicTweaker.java
+++ src/main/java/trinsdar/ic2classictweaker/IC2ClassicTweaker.java
@@ -1,10 +1,75 @@
 package trinsdar.ic2classictweaker;
 
+import ic2.api.classic.recipe.ClassicRecipes;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
 
 @Mod(modid = IC2ClassicTweaker.MODID, name = IC2ClassicTweaker.MODNAME, version = IC2ClassicTweaker.VERSION, dependencies = "required-after:ic2;required-after:ic2-classic-spmod;required-after:crafttweaker;before:jei")
 public class IC2ClassicTweaker {
     public static final String MODID = "ic2_classic_tweaker";
     public static final String MODNAME = "IC2 Classic Tweaker";
     public static final String VERSION = "@VERSION@";
+
+    public static Logger logger;
+    public static Configuration config;
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event){
+        logger = event.getModLog();
+        File directory = event.getModConfigurationDirectory();
+        config = new Configuration(new File(directory.getPath(), "ic2/ic2classictweaker.cfg"));
+        Config.readConfig();
+    }
+
+    @Mod.EventHandler
+    public static void init(FMLInitializationEvent event){
+        initRemoveCanningMachineRecipes();
+        initRemoveRareEarthRecipes();
+    }
+
+    private static void initRemoveRareEarthRecipes(){
+        if (Config.disableAllDefaultRareEarthRecipes){
+            ClassicRecipes.earthExtractor.removeEntry(new ItemStack(Blocks.COBBLESTONE), new ItemStack(Blocks.STONE, 1, 1), new ItemStack(Blocks.STONE, 1, 3), new ItemStack(Blocks.STONE, 1, 5), new ItemStack(Blocks.NETHERRACK), new ItemStack(Blocks.MYCELIUM), new ItemStack(Items.CLAY_BALL), new ItemStack(Blocks.OBSIDIAN), new ItemStack(Blocks.SOUL_SAND));
+        }else {
+            if (Config.disableCobblestone){
+                ClassicRecipes.earthExtractor.removeEntry(new ItemStack(Blocks.COBBLESTONE));
+            }
+            if (Config.disableGranite){
+                ClassicRecipes.earthExtractor.removeEntry(new ItemStack(Blocks.STONE, 1, 1));
+            }
+            if (Config.disableDiorite){
+                ClassicRecipes.earthExtractor.removeEntry(new ItemStack(Blocks.STONE, 1, 3));
+            }
+            if (Config.disableAndestie){
+                ClassicRecipes.earthExtractor.removeEntry(new ItemStack(Blocks.STONE, 1, 5));
+            }
+            if (Config.disableNetherrack){
+                ClassicRecipes.earthExtractor.removeEntry(new ItemStack(Blocks.NETHERRACK));
+            }
+            if (Config.disableMycelium){
+                ClassicRecipes.earthExtractor.removeEntry(new ItemStack(Blocks.MYCELIUM));
+            }
+            if (Config.disableClay){
+                ClassicRecipes.earthExtractor.removeEntry(new ItemStack(Items.CLAY_BALL));
+            }
+            if (Config.disableObsidian){
+                ClassicRecipes.earthExtractor.removeEntry(new ItemStack(Blocks.OBSIDIAN));
+            }
+            if (Config.disableSoulSand){
+                ClassicRecipes.earthExtractor.removeEntry(new ItemStack(Blocks.SOUL_SAND));
+            }
+        }
+    }
+
+    private static void initRemoveCanningMachineRecipes(){
+
+    }
 }
